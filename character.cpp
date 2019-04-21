858f7a7134f696bd6874b441762ff90cf00c369a
Mary Woolley <marymaryalma@gmail.com>
2019-04-21 18:38:02 -0400

    start on character class

--- character.cpp
+++ character.cpp
@@ -2,4 +2,31 @@
 // Created by Mary Woolley on 2019-04-21.
 //
 
+#include <string>
 #include "character.h"
+
+using namespace std;
+
+Character::Character() {
+
+}
+
+void Character::draw() {
+
+}
+
+void Character::move(double x, double y) {
+
+}
+
+void Character::resetPosition() {
+
+}
+
+//int Character::getBottomY() {
+//
+//}
+//
+//int Character::getCentY() {
+//
+//}
--- character.h
+++ character.h
@@ -5,8 +5,33 @@
 #ifndef FINALPROJECT_CHARTREUSE_CHARACTER_H
 #define FINALPROJECT_CHARTREUSE_CHARACTER_H
 
+#include <vector>
 
-class character {
+#include "longSquare.h"
+#include "circle.h"
+
+using namespace std;
+
+class Character {
+private:
+    //vector of body parts
+    vector<Shape*> body;
+    LongSquare torso, arms, legs;
+    Circle head;
+
+public:
+    //constructors
+    Character();
+
+    //other methods
+    void draw();
+    void move(double x, double y);
+    void resetPosition();
+
+    //getters and setters
+    int getBottomY();
+    int getCentY();
+
 
 };
 
