095c4c6c23f70599ac092fe357559b7034ce5e18
VanessaWhite26 <Khfftwewy26>
2019-04-23 11:11:51 -0400

    Added a .cpp and .h for the ghost, token, and key

---
+++ ghost.h
@@ -0,0 +1,14 @@
+//
+// Created by vgwse on 4/23/2019.
+//
+
+#ifndef FINALPROJECT_CHARTREUSE_GHOST_H
+#define FINALPROJECT_CHARTREUSE_GHOST_H
+
+
+class ghost {
+
+};
+
+
+#endif //FINALPROJECT_CHARTREUSE_GHOST_H
